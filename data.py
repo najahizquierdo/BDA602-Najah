72bda83f63b5c9b6834e4b3031e542158e3d800e
najahizquierdo <najahizquierdo@gmail.com>
2022-09-08 00:03:45 -0700

    remove extra transform

--- data.py
+++ data.py
@@ -73,11 +73,12 @@ print(f"Mean accuracy: {classifier_d.score(X_test, y_test)}")
 
 # Pipeline
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(iris_stats, y, random_state=42)
 
 rt_pipeline = Pipeline(
     [("scaler", StandardScaler()), ("rand_tree", RandomForestClassifier())]
 )
+
 rt_pipeline.fit(X_train, y_train)
 pipe_score = rt_pipeline.score(X_test, y_test)
 pipe_predict = rt_pipeline.predict(X_test)
