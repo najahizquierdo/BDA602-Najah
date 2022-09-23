b257f245570c9cd31da7c397836d205636e3313b
najahizquierdo <najahizquierdo@gmail.com>
2022-09-23 10:52:12 -0700

    final rolling average

--- homework2/baseballscripts.sql
+++ homework2/baseballscripts.sql
@@ -1,12 +1,12 @@
-CREATE TABLE historical_average(
-  select batter,
-  cast(round(SUM(Hit)/SUM(atBat), 3) as float) AS batting_average
+CREATE TABLE historicalAverage(
+  select batter
+  , cast(round(NULLIF(SUM(batter_counts.Hit),0)/NULLIF(SUM(batter_counts.atBat),0), 3) as float) AS batting_average
   from batter_counts
   group by batter
   order by batting_average desc
 );
 
-CREATE TABLE annual_average(
+CREATE TABLE annualAverage(
   select batter_counts.batter
   , EXTRACT(year from game.local_date) as batting_year
   , cast(round(NULLIF(SUM(batter_counts.Hit),0)/NULLIF(SUM(batter_counts.atBat),0), 3) as float) AS batting_average
@@ -14,4 +14,23 @@ CREATE TABLE annual_average(
   join game
   on batter_counts.game_id = game.game_id
   group by batter_counts.batter, batting_year asc
+);
+
+CREATE TEMPORARY TABLE last100Games(
+  select game_id
+  , local_date
+  from game
+  where local_date >= (DATE(NOW()) - INTERVAL 100 DAY)
+  order by local_date DESC
+);
+
+CREATE TABLE rollingAverage(
+  select batter_counts.batter
+  , cast(round(NULLIF(SUM(batter_counts.Hit),0)/NULLIF(SUM(batter_counts.atBat),0), 3) as float) AS batting_average
+  , batter_counts.game_id
+  from batter_counts
+  join last100Games
+  on batter_counts.game_id = last100Games.game_id
+  group by batter_counts.batter
+  order by batter,last100Games.local_date, batting_average
 );
